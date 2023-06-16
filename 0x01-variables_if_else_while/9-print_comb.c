aab46e0d0686220c51150f255a49d080e75e2439
ihssane <errajy.ihssane@gmail.com>
2023-06-16 16:10:02 -0700

    Creating 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always return (0) Success
+ */
+int main(void)
+{
+	int dgt;
+
+	for (dgt = '0'; dgt <= '9'; dgt++)
+{
+	putchar(dgt);
+		if (dgt != '9')
+{
+		putchar(',');
+		putchar(' ');
+}
+}
+	putchar('\n');
+	return (0);
+}
