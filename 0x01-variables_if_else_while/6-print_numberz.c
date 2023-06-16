5be38b872479301b8172d005e6a7a918814c3ebc
ihssane <errajy.ihssane@gmail.com>
2023-06-16 15:07:00 -0700

    creating 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always return (0) Success
+ */
+int main(void)
+{
+	int dgt;
+
+	for (dgt = '0'; dgt <= '9'; dgt++)
+{
+	putchar(dgt);
+}
+	putchar('\n');
+	return (0);
+}
