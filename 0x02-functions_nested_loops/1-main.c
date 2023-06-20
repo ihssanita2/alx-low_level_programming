2cecc798c48a608c0cb4e60d21bae09a1c4d710b
ihssane <errajy.ihssane@gmail.com>
2023-06-20 09:53:21 -0700

    editing main.h 1-alphabet.c 1-main.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,16 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet.
- * Return: void.
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+	print_alphabet();
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+
 int _putchar(char c);
 int _isalpha(int c);
 void print_alphabet(void);
+
+#endif
