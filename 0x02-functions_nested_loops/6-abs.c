353dac446913ff2beaaf6f09c35f34cdb9aa2eff
ihssane <errajy.ihssane@gmail.com>
2023-06-20 11:55:39 -0700

    creating 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @j: type int
+ * Return: The absolute value of an integer.
+ */
+int _abs(int j)
+{
+	if (j < 0)
+	{
+	return (j * -1);
+	}
+	else
+	return (j);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,12 @@
 #ifndef _PUTCHAR_H
 #define _PUTCHAR_H
 
-int _putchar(char c);
-int _isalpha(int c);
+int _putchar(char);
+int _isalpha(int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
 #endif
