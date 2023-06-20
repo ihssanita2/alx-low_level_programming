65c4c30bd4a3fe18df60303ea4736f13f29ad89f
ihssane <errajy.ihssane@gmail.com>
2023-06-20 10:09:58 -0700

    creating 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
+	_putchar('\n');
+	}
+}
