c928477b14afd81a36d571a0d5452a1cf4420e4f
ihssane <errajy.ihssane@gmail.com>
2023-06-22 08:18:55 -0700

    Creating 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0 is a Success.
+ */
+int main(void)
+{
+	unsigned long fbn, lsd1, lsd2, sumf;
+
+	lsd1 = 1;
+	lsd2 = 2;
+	fbn = 0;
+	while (fbn < 399997)
+	{
+		fbn = lsd1 + lsd2;
+		if ((fbn % 2) == 0)
+		{
+		sumf += fbn;
+		}
+		lsd1 = lsd2;
+		lsd2 = fbn;
+	}
+	printf("%lu\n", sumf);
+		return (0);
+}
