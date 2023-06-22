aa4269fadabfbe438bf9691b404ee0cf2add4463
ihssane <errajy.ihssane@gmail.com>
2023-06-22 06:37:30 -0700

    Editing 2 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,19 +9,11 @@ int main(void)
 {
 	int x, sum;
 
-	for (x = -500; x < 1024; x++)
+	for (x = 1; x < 1024; x++)
 	{
 		if ((x % 3) == 0 || (x % 5) == 0)
 		{
-			if (x < 0)
-			{
-				x *= -1;
-				sum += x;
-			}
-			else
-			{
 			sum += x;
-			}
 		}
 	}
 			printf("%d\n", sum);
