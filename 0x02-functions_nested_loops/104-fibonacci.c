a04dd280623f683f20dbd4ac60e2e59505fd7e0c
ihssane <errajy.ihssane@gmail.com>
2023-06-22 08:35:05 -0700

    Creating 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0 is a Success.
+ */
+int main(void)
+{
+	int long fbn, lsd1, lsd2;
+	int i;
+
+	lsd1 = 1;
+	lsd2 = 2;
+	printf("%ld, %ld, ", lsd1, lsd2);
+	for (i = 1; i <= 97; i++)
+	{
+		fbn = lsd1 + lsd2;
+		if (i == 97)
+		{
+		printf("%ld", fbn);
+		}
+		else
+		{
+		printf("%ld, ", fbn);
+		}
+		lsd1 = lsd2;
+		lsd2 = fbn;
+	}
+	printf("\n");
+	return (0);
+}
