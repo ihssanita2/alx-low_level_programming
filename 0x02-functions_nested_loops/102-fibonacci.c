f516fdab6a83d2b2fb2db15a627bc609522ea6ee
ihssane <errajy.ihssane@gmail.com>
2023-06-22 07:47:56 -0700

    Creating 1 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,23 @@
  */
 int main(void)
 {
-	int i; 
-	int long fbn, lsd1 = 1, lsd2 = 2;
+	int long fbn, lsd1, lsd2;
+	int i;
 
+	lsd1 = 1;
+	lsd2 = 2;
 	printf("%ld, %ld, ", lsd1, lsd2);
-	for (i = 0; i <= 50; i++)
+	for (i = 1; i <= 48; i++)
 	{
 		fbn = lsd1 + lsd2;
+		if (i == 48)
+		{
+		printf("%ld", fbn);
+		}
+		else
+		{
 		printf("%ld, ", fbn);
+		}
 		lsd1 = lsd2;
 		lsd2 = fbn;
 	}
