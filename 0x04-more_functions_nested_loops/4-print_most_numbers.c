67c657a427840c900468fcef135891142e12d5c2
ihssane <errajy.ihssane@gmail.com>
2023-06-22 18:55:47 -0700

    CR 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,13 @@
  * print_most_numbers - prints the numbers, from 0 to 9 expet 2 and 4.
  * Return: void.
  */
-void print_most_numbers(void))
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 || i != 4)
+		if (i != 2 && i != 4)
 		{
 		_putchar(i + '0');
 		}
