eaf335393b3af40180594df5ad55f74dc5a58da5
ihssane <errajy.ihssane@gmail.com>
2023-06-23 07:00:07 -0700

    CR 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,21 @@
 void more_numbers(void)
 {
 	int i, j, x;
-
-	for (i = 0; i <= 10; i++)
+for (i = 1; i <= 10; i++)
+{
+	for (j = 0; j <= 14; j++)
 	{
-		for (j = 0; j <= 14; j++)
+		if (j >= 0 && j <= 9)
 		{
-			if (j >= 0 && j <= 9)
-			{
-				x = j + 10;
-			}
-			else if (j >= 10 && j <= 14)
-			{
-				_putchar('1');
-				x = j;
-			}
-			_putchar((x % 10) + '0');
+			x = j + 10;
 		}
-	_putchar('\n');
+		else if (j >= 10 && j <= 14)
+		{
+			_putchar('1');
+			x = j;
+		}
+		_putchar((x % 10) + '0');
 	}
+_putchar('\n');
+}
 }
