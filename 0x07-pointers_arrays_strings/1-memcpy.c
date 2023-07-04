bd9c35a02edbde7ad10fd825b9bc48cea6252746
ihssane <errajy.ihssane@gmail.com>
2023-07-04 16:58:20 -0700

    ED1 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int count;
 
 	for (count = 0; count < n; count++)
-		*dest++ = *src++;
+		dest[count] = src[count];
 
 	return (dest);
 }
