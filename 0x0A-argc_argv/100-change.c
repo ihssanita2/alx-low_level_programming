f08e0400d59dd7d07858a39160728d6040332a5e
ihssane <errajy.ihssane@gmail.com>
2023-07-08 04:44:10 +0100

    ED 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,17 +10,16 @@
 */
 int main(int argc, char *argv[])
 {
-int N, i;
-
-if ((argc - 1) != 2)
+if (argc != 2)
 {
+
 printf("Error\n");
 return (1);
 }
-N = atoi(argv[i]);
-if (N < 0)
+if (atoi(argv[1]) < 0)
 {
 printf("0\n");
+return (0);
 }
 return (0);
 }
