54e840f10c1d947429135aed6eb486732309c52a
ihssane <errajy.ihssane@gmail.com>
2023-07-11 18:09:06 +0100

    Creating 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+int _strlen_recursion(char *s);
+
+/**
+* str_concat - concatenates two strings.
+* @s1: The first string.
+* @s2: The second string.
+* Return: pointer contanet the two strings, return NULL on failure.
+*/
+char *str_concat(char *s1, char *s2)
+{
+unsigned int i, j, size;
+char *Array;
+
+size = _strlen_recursion(s1) + _strlen_recursion(s2) + 1;
+Array = (char *)malloc(size);
+if (Array == NULL)
+{
+return (NULL);
+}
+if (s1 == NULL || s2 == NULL)
+{
+*Array = '\0';
+}
+for (i = 0; s1[i] != '\0'; i++)
+{
+Array[i] = s1[i];
+}
+for (j = 0; s2[j] != '\0'; j++)
+{
+Array[i] = s2[j];
+i++;
+}
+if (Array[i] != '\0')
+{
+i++;
+Array[i] = '\0';
+}
+return (Array);
+}
+/**
+*_strlen_recursion - returns the length of a string.
+*@s: the string to mesure.
+*Return:  the length of a string.
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	return (0);
+}
