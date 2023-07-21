9609aa1ae8e9534935a0a534f0c4a9d07b0632ff
ihssane <errajy.ihssane@gmail.com>
2023-07-21 19:11:26 +0100

    Creating 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/*Ihssane ERRAJY*/
+/**
+ * print_name - prints a name.
+ * @name: name to be printed.
+ * @f: function pointer.
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+{
+f(name);
+}
+}
+
