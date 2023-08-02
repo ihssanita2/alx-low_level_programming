92a4f8457b0e63a6461c9c8dd06b9bdcb071684f
ihssane <errajy.ihssane@gmail.com>
2023-08-02 22:59:11 +0100

    Creating 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/*ihssaneer*/
+/**
+ * executed_before_main - function that prints message
+ * before the main function is executed.
+ * Return: void.
+*/
+void executed_before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
