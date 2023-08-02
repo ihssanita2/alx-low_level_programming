78126ffab62feb280b1852f2e5bb01257a0836f7
ihssane <errajy.ihssane@gmail.com>
2023-08-02 17:34:23 +0100

    Editing 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = tmp->next;
 	i++;
 	}
-	if (tmp->next == NULL)
+	if (tmp->next == NULL && idx > i)
 	{
 		free(new);
 		return (NULL);
