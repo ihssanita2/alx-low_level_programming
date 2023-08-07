bc315f479580d1f74425ffc3ab05d8962b404d1e
ihssane <errajy.ihssane@gmail.com>
2023-08-07 20:17:32 +0100

    ED 3 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,10 +16,11 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	f1_index = open(argv[1], O_RDONLY);
-	if (f1_index == -1)
+	if (access(argv[1], F_OK) == -1 || access(argv[1], R_OK) == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+	umask(0000);
 	f2_index = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
 	if (f2_index == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -27,15 +28,15 @@ int main(int argc, char *argv[])
 	}
 	while ((r_bites = read(f1_index, buffer, BUF_1024)) > 0)
 	{	w_bites = write(f2_index, buffer, r_bites);
-		if (r_bites != w_bites)
+		if (w_bites == -1)
 		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-		if (r_bites == -1)
-		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+	if (r_bites == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(f1_index) == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1_index);
 		exit(100);
