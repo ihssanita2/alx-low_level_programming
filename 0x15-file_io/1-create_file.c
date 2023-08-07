b221a3f6654b2b65372708c9f78d28ff5d766ece
ihssane <errajy.ihssane@gmail.com>
2023-08-07 16:21:19 +0100

    ED 1 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,7 @@ int create_file(const char *filename, char *text_content)
 	/*code section : */
 	if (filename == NULL)
 		return (-1);
+
 	umask(0000);
 	f_index = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (f_index == -1)
@@ -27,5 +28,5 @@ int create_file(const char *filename, char *text_content)
 	}
 	close(f_index);
 	/* return section :*/
-	return (f_index);
+	return (1);
 }
